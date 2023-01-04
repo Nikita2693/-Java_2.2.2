1e62dc9767a4961b3093baff784061e327315e2f
ExPress <belkinnik93@gmail.com>
2023-01-04 17:01:16 +0300

    first commit

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+    public double calculate( double weight, double height) {
+
+        if (height <= 0) return 0;
+
+        return weight / Math.pow(height, 2);
+    }
+}
